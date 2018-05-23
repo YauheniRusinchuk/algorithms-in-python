c72205d0012a671b2841063494c10cbd10bb36fb
YauheniRusinchuk <ruevgal@gmail.com>
2018-05-23 11:21:56 +0300

    Add generat nubmer

---
+++ number_generator.py
@@ -0,0 +1,21 @@
+def generation_nubmer(N:int,M:int, prefix=None):
+    ''' Генерирует все чилса (с лидирующими незначащими нулями)
+    в N - ричной системе счисления(N<10)
+    длины M
+    '''
+    prefix = prefix or []
+    if M == 0:
+        print(prefix)
+        return
+    for digit in range(N):
+        prefix.append(digit)
+        generation_nubmer(N, M-1, prefix)
+        prefix.pop()
+
+
+def gen_bin(M, prefix=""):
+    if M == 0:
+        print(prefix)
+    else:
+        gen_bin(M-1,prefix+"0")
+        gen_bin(M-1,prefix+"1")
